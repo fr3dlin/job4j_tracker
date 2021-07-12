ff1cab37ad0a8007cd194d9b9839c4eac3d6760a
fr3dlin <fr3dlin@gmail.com>
2021-07-12 21:04:35 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,14 +1,36 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.name + " eat " + this.food);
+    }
+
     public String sound() {
-        String voice = " may-may";
-        return voice;
+        return "may-may";
     }
 
     public static void main(String[] args) {
         Cat perry = new Cat();
         String say = perry.sound();
         System.out.println("Perry says " + say);
+        perry.giveNick("Perry");
+        perry.eat("kotleta");
+        perry.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
