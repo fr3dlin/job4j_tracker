47ad50e99faa95cd2a117d2c748f996705e94b7c
fr3dlin <fr3dlin@gmail.com>
2021-07-15 12:04:49 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student stdu = new Student();
+        stdu.setFio("Arsenev Petr Mickhailovich");
+        stdu.setGroup("AP32");
+        stdu.setDate(new Date());
+
+        System.out.println(stdu.getFio() + " in group - " + stdu.getGroup());
+    }
+}
