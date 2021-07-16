6d576b0c5ad9af9ef51227a5827b6fc262aa141b
fr3dlin <fr3dlin@gmail.com>
2021-07-16 16:12:13 +0300

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class StartUI {
-    @SuppressWarnings("checkstyle:ParenPad")
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
@@ -26,6 +25,18 @@ public class StartUI {
                     }
                 } else {
                     System.out.println("Хранилище не содержит заявок");
+                }
+            } else if (select == 2) {
+                System.out.println("=== Edit item ====");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Заявка изменена успешно.");
+                } else {
+                    System.out.println("Ошибка замены заявки.");
                 }
             } else if (select != 6) {
                 System.out.println("Пользователь выбрал: " + select);
