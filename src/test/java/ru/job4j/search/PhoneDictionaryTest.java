767ca0bfb91091ee088e3ba2e6d41caa11d3879a
fr3dlin <fr3dlin@gmail.com>
2021-08-02 11:21:19 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #183555]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -16,4 +16,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Pet");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    @Test
+    public void whenFindByNameInvlid() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("SDf");
+        assertThat(persons.size(), is(0));
+    }
 }
