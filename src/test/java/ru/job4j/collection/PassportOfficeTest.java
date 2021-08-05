e2b08056169ebe80a6ba083c17896b70a3152dd0
fr3dlin <fr3dlin@gmail.com>
2021-08-05 11:31:34 +0300

    1. Паспорт и жители [#212656 #185274]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -14,4 +14,14 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        boolean rsl = office.add(citizen2);
+        assertThat(rsl, is(false));
+    }
 }
