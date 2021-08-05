f13df81585abee4140383116deffff36c6590613
fr3dlin <fr3dlin@gmail.com>
2021-08-05 14:29:20 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] leftStr = o1.split(". ");
+        String[] rightStr = o2.split(". ");
+
+        int left = Integer.parseInt(leftStr[0]);
+        int right = Integer.parseInt(rightStr[0]);
+        return left - right;
+
+    }
+}
