55368d97f7dd016ddf8132e0ef6992e9d7f04374
fr3dlin <fr3dlin@gmail.com>
2021-08-23 17:33:42 +0300

    1.2. Функциональный интерфейс [#247497 #193390]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -15,13 +15,12 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        Predicate<Integer> pred = (i) -> i % 2 == 0 || map.get(i).length() == 4;
+        BiPredicate<Integer, String> pred = (i, s) -> i % 2 == 0 || s.length() == 4;
 
         for (Integer i : map.keySet()) {
-            if (pred.test(i)) {
+            if (pred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
-
         }
 
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
