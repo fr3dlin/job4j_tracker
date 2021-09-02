dea52c3e014b8049f7498172725b8761f0023eff
fr3dlin <fr3dlin@gmail.com>
2021-09-02 11:29:16 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -9,16 +9,16 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -26,7 +26,7 @@ public class DepartmentsTest {
     @Test
     public void sortAsc() {
         List<String> input = Arrays.asList("k1", "k2", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2");
+        List<String> expect = List.of("k1", "k1/sk1", "k2");
         Departments.sortAsc(input);
         assertThat(input, is(expect));
     }
@@ -34,7 +34,7 @@ public class DepartmentsTest {
     @Test
     public void sortDesc() {
         List<String> input = Arrays.asList("k1", "k2", "k1/sk1");
-        List<String> expect = Arrays.asList("k2", "k1", "k1/sk1");
+        List<String> expect = List.of("k2", "k1", "k1/sk1");
         Departments.sortDesc(input);
         assertThat(input, is(expect));
     }
--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -2,7 +2,6 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,12 +12,12 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
@@ -28,14 +27,14 @@ public class NotifyAccountTest {
 
     @Test
     public void sentDouble() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"),
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("123", "Petr Arsentev", "eDer3432f")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
